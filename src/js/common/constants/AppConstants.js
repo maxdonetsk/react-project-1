84bd85e586ae634f29fa612030e457fd4b59ea2e
maxdonetsk <max.donetsk@mail.ru>
2017-04-15 16:19:09 +0300

    DOMAIN wariable changed to location.hostname

--- src/js/common/constants/AppConstants.js
+++ src/js/common/constants/AppConstants.js
@@ -62,7 +62,7 @@ module.exports = {
   AUTHENTICATION_COOKIE_NAME: 'token',
   USER_ID_COOKIE_NAME: 'uuid',
   USER_OBJECT_STORAGE_NAME: 'user',
-  DOMAIN: __DEV__ ? 'localhost' : 'producer.veles.trade',
+  DOMAIN: location.hostname,
   PATH: '/',
   SECURE: !__DEV__
 };
